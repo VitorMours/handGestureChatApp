5daf7e86cb46df7102a32a1ba5f5466d60576150
João Vitor Rezende Moura <72020085+VitorMours@users.noreply.github.com>
2025-05-07 18:43:09 -0300

    Update views.py

--- views.py
+++ views.py
@@ -1,25 +1,49 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for
+from flask_socketio import SocketIO, emit, send, leave_room, join_room
 
 bp = Blueprint("bp", __name__, template_folder="template")
+rooms = {}
+socketio = SocketIO()
 
 @bp.route("/base", methods=["GET"])
 def base():
   return render_template("base.html")
 
-@bp.route("/room/<room_code>/", methods=["GET", "POST"])
+@bp.route("/room/<room_code>")
 def room(room_code):
-  if request.method == "GET":
+    room_code = session["room_code"]
+    name= session["name"]
+
+    if room_code not in rooms or name is None or room_code is None:
+      return render_template(url_for("index"))
+    
     return render_template("room.html")
-  elif request.method == "POST":
-    return render_template("room.html", room_code=room_code)
-
-
 
 @bp.route("/", methods=["GET","POST"])
 def index():
+  session.clear()
   if request.method == "GET":
     return render_template("index.html")
   if request.method == "POST":
-    session["name"] = request.form.get("name")
+    name = request.form.get("name")
     room_code = request.form.get("room_code")
-    return redirect(url_for("bp.room", room_code=room_code))
+    session["room_code"] = room_code
+    session["name"] = name
+    print(f"The user: {name} enter the room: {room_code}")
+    return redirect(url_for("bp.room", room_code = room_code))
+  
+
+
+@socketio.on("connect")
+def conection_handler(auth):
+  name = session["name"]
+  room_code = session["room_code"]
+
+  if not room or not name:
+    return 
+  if room not in rooms:
+    leave_room(room)
+    return 
+  join_room(room)
+  send({"name" : name, "message": "has entered the room"}, to=room_code)
+  rooms[room]["members"] += 1
