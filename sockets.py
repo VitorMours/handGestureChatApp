c9d76d5b91982bd4934d50099c85acdc1d2450d2
João Vitor Rezende Moura <72020085+VitorMours@users.noreply.github.com>
2025-05-07 18:43:26 -0300

    Update sockets.py

--- sockets.py
+++ sockets.py
@@ -1,19 +1,1 @@
-from flask_socketio import SocketIO 
-
-socketio = SocketIO()
-
-@socketio.on("connect")
-def connection_created():
-    pass
-
-@socketio.on("disconnect")
-def connection_killed():
-    pass 
-
-@socketio.on("message")
-def message(message):
-    pass
-
-
-
 
