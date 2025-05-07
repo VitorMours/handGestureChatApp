9dca350fa90778ef0729384826d2727eebcef9e4
João Vitor Rezende Moura <72020085+VitorMours@users.noreply.github.com>
2025-05-07 18:43:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template # type: ignore
 from views import bp
-from sockets import socketio
-
+from views import socketio
 
 def create_app() -> Flask:
   app = Flask(__name__)
   app.config["SECRET_KEY"] = "secret_key_for_server_in_websockets"
-
   app.register_blueprint(bp)
   socketio.init_app(app)
   return app
